f5ae763d053189453dc0625cfcac6f2c25f0eb5b
Saskia <bobinska.dev@gmail.com>
2024-08-07 20:17:51 +0200

    adjustment image header section -> light mode

--- sanity/components/sections/previews/ImageHeaderSectionPreview.tsx
+++ sanity/components/sections/previews/ImageHeaderSectionPreview.tsx
@@ -1,7 +1,8 @@
 import { ImageHeaderSection } from '@/sanity.types'
 import { urlForImage } from '@/sanity/lib/utils'
 import { ImageIcon } from '@sanity/icons'
-import { Box, Card, Stack, Text } from '@sanity/ui'
+import { Box, Flex, Stack, Text } from '@sanity/ui'
+import Image from 'next/image'
 import { ComponentType } from 'react'
 import { Image as ImageType, PreviewProps } from 'sanity'
 import ContentArrayPreviewContainer from '../container/PreviewContainer'
@@ -11,11 +12,11 @@ type ImageHeaderSectionPreviewProps = PreviewProps & ImageHeaderSection
 const ImageHeaderSectionPreview: ComponentType<
   ImageHeaderSectionPreviewProps
 > = (props) => {
-  const width = 400
-  const height = 200
+  const width = 300
+  const height = 150
   const image = props.image as ImageType
   const imageUrl = image
-    ? urlForImage(image)?.width(width).height(height).fit('fill').url()
+    ? urlForImage(image)?.width(width).height(height).url()
     : ''
 
   return (
@@ -25,52 +26,29 @@ const ImageHeaderSectionPreview: ComponentType<
       body="" //{props.subtitle as string}
       icon={<ImageIcon />}
     >
-      <Card
-        style={{
-          width: '100%',
-          height: '100%',
-          backgroundSize: 'cover',
-          backgroundBlendMode: 'multiply',
-          background: `url("${
-            imageUrl ||
-            `https://cdn.sanity.io/images/zvy7mysv/production/d06087e3a2bb8c0760cdefdfa1be504a360c3f34-1253x681.webp?w=${width}&h=${height}&fit=max`
-          }") rgba(19, 20, 27, 0.971)`,
-        }}
-        radius={2}
-        shadow={1}
-      >
-        <Stack
-          space={4}
-          style={{
-            height: '100%',
-            width: '100%',
-          }}
-          paddingX={4}
-          paddingY={5}
-        >
+      <Flex align={'center'} paddingX={4} paddingY={3}>
+        <Stack space={4}>
           <Box>
-            <Text weight="bold" size={3}>
-              {props.title as string}
-            </Text>
+            <h2>{props.title as string}</h2>
           </Box>
           <Box>
-            <Text weight="semibold" size={1}>
+            <Text size={1} muted>
               {props.subtitle as string}
             </Text>
           </Box>
         </Stack>
-      </Card>
-      {/*       <Box>
-        <Image
-          src={
-            imageUrl ||
-            `https://cdn.sanity.io/images/zvy7mysv/production/d06087e3a2bb8c0760cdefdfa1be504a360c3f34-1253x681.webp?w=${width}&h=${height}&fit=max`
-          }
-          alt={props.title as string}
-          width={width}
-          height={height}
-        />
-      </Box> */}
+        <Box>
+          <Image
+            src={
+              imageUrl ||
+              `https://cdn.sanity.io/images/zvy7mysv/production/d06087e3a2bb8c0760cdefdfa1be504a360c3f34-1253x681.webp?w=${width}&h=${height}&fit=max`
+            }
+            alt={props.title as string}
+            width={width}
+            height={height}
+          />
+        </Box>
+      </Flex>
     </ContentArrayPreviewContainer>
   )
 }
